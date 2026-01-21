54d38cc35cc9dedf15b6578a791fd1ad56b56c76
Swayam Kelkar <67506271+Ecstaticvanilla@users.noreply.github.com>
2026-01-21 18:57:52 +0530

    get notes and url

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,3 +1,42 @@
-chrome.action.onClicked.addListener((tab) => {
-    console.log(tab.url);
+
+// function getword(info,tab) {
+//   console.log("Word " + info.selectionText + " was clicked.");
+//   chrome.tabs.create({  
+//     url: "http://www.google.com/search?q=" + info.selectionText
+//   });
+// }
+const CONTEXT_MENU_ID = "ADD_NOTE";
+// function getword(info,tab) {
+
+//   console.log("Word " + info.selectionText + " was clicked.");
+//   chrome.tabs.create({  
+//     url: "http://www.google.com/search?q=" + info.selectionText
+//   });
+// }
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.contextMenus.create({
+    title: "Add %s to Notes", 
+    contexts:["selection"], 
+    id: CONTEXT_MENU_ID
+    });
 });
+
+async function addToNotes(info,tab){
+    if (info.menuItemId !== CONTEXT_MENU_ID) {
+        return;
+    }
+    // let queryOptions = { active: true, lastFocusedWindow: true };
+    // let [tab] = await chrome.tabs.query(queryOptions);
+    console.log("Note : \"" + info.selectionText +"\". \nurl : " + tab.url.split('#')[0]);
+    return;
+}
+
+chrome.contextMenus.onClicked.addListener(addToNotes)
+
+// async function getCurrentTab(info,tab) {
+
+//     let queryOptions = { active: true, lastFocusedWindow: true };
+//     let [tab] = await chrome.tabs.query(queryOptions);
+//     console.log("context menu : " + tab.url);
+//     return tab;
+// }
---
+++ extension/js/content.js
@@ -0,0 +1,8 @@
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    console.log(tab);
+    return tab;
+}
+
+getCurrentTab();
