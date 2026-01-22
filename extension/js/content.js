98a4bb06bde55b06aee37cb56be7f14afc3a8027
Pirate-Programmer <harshmm25@gmail.com>
2026-01-22 21:03:54 +0530

    adding message passing

--- extension/js/content.js
+++ extension/js/content.js
@@ -6,3 +6,32 @@ async function getCurrentTab() {
 }
 
 // getCurrentTab();
+
+
+async function createNewNote(){
+
+}
+
+//recieve message from popup.js for note creation
+chrome.runtime.onMessage.addListener(async (request) => {
+    console.log("in the contentjs")
+    if (request.action === 'createnote') {
+        const note = document.createElement("textarea");
+        note.style.backgroundColor="black";
+        // note.style.height="100px";
+        // note.style.width="100px";
+        note.style.position="relative";
+        note.style.top="0px";
+        note.style.left="100px";
+        note.style.zIndex="4000";
+        note.innerText="THIS IS A NOTE!!!";
+        note.id="10";
+        
+        note.style.resize="both";
+
+        document.head.appendChild(note);
+        console.log("created note");
+    }
+    
+    return true;
+});
